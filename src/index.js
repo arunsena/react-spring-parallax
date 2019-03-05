e2fb99cb8690415a500f0d7b9a7aeda37899d18d
Arun Bojja <arunsena123@gmail.com>
2019-03-05 00:20:05 -0500

    Adding parallax components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons';
 
 const App = () => {
-    return <h1>Hello I am from react simple component</h1>;
+    return (
+        <div className="ui container-fluid">
+         <h1 className="ui header">React spring Parallax</h1>
+         <Parallax pages={3}>
+             <ParallaxLayer offset={0} speed={0.5} factor={1.5} style={{backgroundColor: '#aaa'}}>
+                 <div>Layer 1 is going here!!</div> 
+             </ParallaxLayer>
+             <ParallaxLayer offset={1} speed={0.5} style={{backgroundColor: '#eee'}}>
+                 <div>Layer 2 is going here!!</div>
+             </ParallaxLayer>
+             <ParallaxLayer offset={2} speed={0.5} style={{backgroundColor: '#ddd'}}>
+                 <div>Layer 3 is going here!!</div>
+             </ParallaxLayer>
+         </Parallax>
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
