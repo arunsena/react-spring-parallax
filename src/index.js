9de49fd0cca62f0657c59ea828e9c6b6b865951b
Arun Bojja <arunsena123@gmail.com>
2019-03-04 23:01:45 -0500

    add react spring dependency

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>Hello I am from react simple component</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
