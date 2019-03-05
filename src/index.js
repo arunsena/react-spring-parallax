4d0e76d1a878006cdd1b2c89577a191eb0473caf
Arun Bojja <arunsena123@gmail.com>
2019-03-05 00:31:03 -0500

    faker images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons';
 
 const App = () => {
@@ -8,13 +9,13 @@ const App = () => {
          <h1 className="ui header">React spring Parallax</h1>
          <Parallax pages={3}>
              <ParallaxLayer offset={0} speed={0.5} factor={1.5} style={{backgroundColor: '#aaa'}}>
-                 <div>Layer 1 is going here!!</div> 
+                 <img className="ui image" src={faker.image.image()} alt=""/>
              </ParallaxLayer>
              <ParallaxLayer offset={1} speed={0.5} style={{backgroundColor: '#eee'}}>
-                 <div>Layer 2 is going here!!</div>
+                 <img className="ui image" src={faker.image.nature()} alt=""/>
              </ParallaxLayer>
              <ParallaxLayer offset={2} speed={0.5} style={{backgroundColor: '#ddd'}}>
-                 <div>Layer 3 is going here!!</div>
+                 <img className="ui image" src={faker.image.image()} alt=""/>
              </ParallaxLayer>
          </Parallax>
         </div>
